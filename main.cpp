a97ffa8fd4c2373ef223cc4c4fadef384a5e6f52
Christoffer Nordeng <panser_bjarne@hotmail.com>
2012-11-01 20:22:04 +0100

    Added random seed

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
+#include <time.h>
 
 int main(int argc, char *argv[])
 {
+    srand(time(0));
+
     QApplication a(argc, argv);
 
     MainWindow w;
